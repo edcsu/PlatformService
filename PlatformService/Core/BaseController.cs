7dcf1934cbeaad56d415a9e1e5601bb2951d7f0c
Sewannonda Keith Edwin <skeith696@gmail.com>
2022-04-11 17:04:59 +0300

    feat: add platform controller

---
+++ PlatformService/Core/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace PlatformService.Core
+{
+    [ApiController]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
+    public class BaseController : ControllerBase
+    {
+    }
+}
