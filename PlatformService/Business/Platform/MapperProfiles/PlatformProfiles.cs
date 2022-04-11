7c62fcea2dd6b714c4fb7cc75f9fcf96bc0e32c6
Sewannonda Keith Edwin <skeith696@gmail.com>
2022-04-11 16:58:47 +0300

    feat: add initial automapper profile

---
+++ PlatformService/Business/Platform/MapperProfiles/PlatformProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using PlatformService.Business.Platform.ViewModels;
+
+namespace PlatformService.Business.Platform.MapperProfiles
+{
+    public class PlatformProfiles : Profile
+    {
+        public PlatformProfiles()
+        {
+            // source -> destination
+            CreateMap<Models.Platform, PlatformDetails>();
+            
+            CreateMap<PlatformCreate, Models.Platform>();
+        }
+    }
+}
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -44,6 +44,8 @@ try
     builder.Services.AddDbContext<ApplicationDbContext>(options =>
        options.UseInMemoryDatabase("inmem"));
 
+    builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
