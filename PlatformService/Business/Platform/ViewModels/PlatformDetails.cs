b0643a5f7dbf0ce8287e1b64e973f4b9c3173294
Sewannonda Keith Edwin <skeith696@gmail.com>
2022-04-11 16:51:26 +0300

    feat: create initial DTOs

---
+++ PlatformService/Business/Platform/ViewModels/PlatformDetails.cs
@@ -0,0 +1,17 @@
+﻿namespace PlatformService.Business.Platform.ViewModels
+{
+    public class PlatformDetails
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Publisher { get; set; }
+
+        public string? Cost { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime Updated { get; set; }
+    }
+}
